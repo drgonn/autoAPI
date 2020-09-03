9bd824a2ff9f8b11c544cede33e6812ebcc197c5
rong <rong.chen@sealan.tech>
2020-09-03 19:22:43 +0800

    group

---
+++ work/stock/src/app/apiv1/Group.py
@@ -0,0 +1,109 @@
+from datetime import date,timedelta,datetime
+import logging
+import math
+import json
+
+from flask import request,jsonify,current_app,g
+from sqlalchemy import func
+from sqlalchemy import not_,or_,and_,extract
+
+from app.apiv1 import api
+from app.standard import Permission
+from app.decorators import admin_required, permission_required
+from app import db
+from app.tools import is_admin,get_permission
+from app.models import Group
+
+@api.route('/group/<int:id>', methods=['GET'])
+def get_group(id):
+	group = Group.query.get_or_404(id)
+
+	return jsonify({'success':True,
+                    'error_code':0,
+                    'records':group.to_json(),
+                    })
+
+@api.route('/group', methods=['POST'])
+def create_group():
+	print(request.json)
+	name = request.json.get('name')
+	if name is None:
+		return jsonify({'success': False, 'error_code': -123, 'errmsg': '缺少必填参数：name'})
+
+	group = Group(name=name,)
+
+	db.session.add(group)
+	try:
+		db.session.commit()
+	except Exception as e:
+		db.session.rollback()
+		logging.error(f'添加数据库发生错误,已经回退:{e}')
+		return jsonify({'success': False, 'error_code': -123, 'errmsg': '数据库插入错误，请查看日志'})
+
+	return jsonify({'success':True,
+                    'error_code':0,
+                    })
+
+@api.route('/group/<int:id>', methods=['PUT'])
+def modify_group(id):
+	group = Group.query.get_or_404(id)
+	name = request.json.get('name')
+	group.name = name or group.name
+	db.session.add(group)
+
+	try:
+		db.session.commit()
+	except Exception as e:
+		db.session.rollback()
+		logging.error(f'修改数据库发生错误,已经回退:{e}')
+	return jsonify({'success':True,
+                    'error_code':0,
+                    })
+
+@api.route('/group', methods=['DELETE'])
+def delete_group():
+	print('delete json:',request.json)
+	ids = request.json.get('ids')
+	for id in ids:
+		group = Group.query.get(id)
+		if group is None:
+			return jsonify({'success': False, 'error_code': -123, 'errmsg': f'删除错误，id： {id} 不存在'})
+	db.session.delete(group)
+
+	try:
+		db.session.commit()
+	except Exception as e:
+		db.session.rollback()
+		logging.error(f'删除数据库发生错误,已经回退:{e}')
+
+	return jsonify({'success':True,
+                'error_code':0,
+                })
+
+@api.route('/group/list', methods=['GET'])
+def list_group():
+	print(request.args)
+	sorter = request.args.get('sorter')
+	page = int(request.args.get('current', 1))
+	pagesize = int(request.args.get('pagesize', current_app.config['PER_PAGE']))
+	pagesize = 20 if pagesize < 10 else pagesize
+	total_groups = Group.query
+	name = request.args.get('name')
+	if name is not None:
+		total_groups = total_groups.filter(Group.name.ilike(f'%{name}%'))
+
+	if sorter:
+		sorter = json.loads(sorter)
+		pass
+	totalcount = total_groups.with_entities(func.count(Group.id)).scalar()
+	page = math.ceil(totalcount/pagesize) if  math.ceil(totalcount/pagesize) < page else page
+	pagination = total_groups.paginate(page, per_page = pagesize, error_out = False)
+	groups = pagination.items
+
+	return jsonify({
+                    'success':True,
+                    'error_code':0,
+                    'total':totalcount,
+                    'data':[group.to_json() for group in groups]
+                    })
+
